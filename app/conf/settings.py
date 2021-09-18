5070481d1a629bbc916e8ebd880b382511178110
Вадим Низовцев <krizon1@yandex.ru>
2021-09-18 23:21:23 +0300

    not db

--- app/conf/settings.py
+++ app/conf/settings.py
@@ -3,14 +3,8 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    DB_USER: str
-    DB_PASSWORD: str
-    DB_NAME: str
     FILE_SAVE_PATH: str
-    PGADMIN_EMAIL: str
-    PGADMIN_PASSWORD: str
-    LOGIN: str
-    PASS: str
+
 
     class Config:
         env_file = ".env"
